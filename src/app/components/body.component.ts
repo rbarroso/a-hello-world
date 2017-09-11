dcbd1d2caae62c0d98464e8f783a4cbfbfd490b3
Rubén Barroso <rbarroso@MacBook-Pro-de-Ruben.local>
2017-09-11 22:23:13 +0200

    first commit

---
+++ src/app/components/body.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: 'body.component.html'
+})
+
+export class BodyComponent {
+
+  mostrar:boolean = false;
+
+  frase:any = {
+    mensaje: "un gran poder requiere una gran responsabilidad",
+    autor: "Ben Parker"
+  }
+
+  personajes:string[] = ["Spiderman", "Venom", "Dr. Octopus"]
+
+}
